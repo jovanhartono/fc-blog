5ca4228d53ec03edf41915d46da035a1b27dfc06
jovan <jovanus@yahoo.co.id>
2023-12-11 21:52:19 +0700

    feat: metadata baseline

---
+++ src/config/metadata.ts
@@ -0,0 +1,24 @@
+export const metadataConfig = {
+  title: "Jasa Cuci & Reparasi Sepatu | Fresclean",
+  description:
+    "Jasa perawatan sepatu profesional. Spesialis dalam membersihkan dan reparasi sepatu. Kunjungi kami untuk perawatan sepatu terbaik.",
+  url: "https://fresclean.id",
+  ogImage: "https://tx.shadcn.com/og.jpg",
+  language: "id",
+  socialBanner: "/logo.jpeg",
+  links: {
+    email: "hello@fresclean.id",
+    instagram: "https://instagram.com/fresclean_id/",
+  },
+  keywords: [
+    "shoe laundry",
+    "shoe cleaning",
+    "shoe restoration",
+    "shoe care",
+    "cuci sepatu",
+    "reparasi sepatu",
+    "laundry sepatu",
+    "cuci helm",
+    "semir sepatu",
+  ],
+};
