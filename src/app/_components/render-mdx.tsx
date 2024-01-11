70beb0736b256f775cd40f22472455937c6fc9f9
jovan <jovanus@yahoo.co.id>
2024-01-11 13:07:51 +0700

    feat: integrate contentlayer with tinacms

---
+++ src/app/_components/render-mdx.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Article } from "contentlayer/generated";
+import { useMDXComponent } from "next-contentlayer/hooks";
+
+import MarkdownImage from "@/app/_components/markdown-image";
+
+export default function RenderMdx({ article }: { article: Article }) {
+  const MDXContent = useMDXComponent(article.body.code);
+
+  return (
+    <section className="article">
+      <MDXContent components={{ img: MarkdownImage }} />
+    </section>
+  );
+}
