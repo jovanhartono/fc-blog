5500af9cd36dbdfb197bd42b48f6b552fd9970c3
jovan <jovanus@yahoo.co.id>
2024-01-15 22:21:36 +0700

    fix(theme switcher): mismatch server and client svg

--- src/app/_components/theme-switcher.tsx
+++ src/app/_components/theme-switcher.tsx
@@ -1,10 +1,36 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 
 export default function ThemeSwitcher() {
-  const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState<boolean>(false);
+  const { setTheme, theme, systemTheme } = useTheme();
+
+  console.log(theme, systemTheme);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return;
+
+  if (theme === "system") {
+    return (
+      <button
+        aria-label="theme-switcher"
+        className="text-black dark:text-white"
+        onClick={() => setTheme(systemTheme === "dark" ? "light" : "dark")}
+      >
+        {systemTheme === "dark" ? (
+          <MoonIcon className="h-4 w-4" />
+        ) : (
+          <SunIcon className="h-4 w-4" />
+        )}
+      </button>
+    );
+  }
 
   return (
     <button
