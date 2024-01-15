3e5a3c98dceaaa11b2a3f311ac3c04a045b7ab6a
jovan <jovanus@yahoo.co.id>
2024-01-15 22:22:20 +0700

    chore: adjust shadow and tw bg variable

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -7,7 +7,7 @@ import HeaderLogo from "../../../public/fc-logo-cropped.jpeg";
 
 export default function Header() {
   return (
-    <header className="fixed left-0 right-0 top-0 z-50 bg-white shadow-sm dark:bg-dark">
+    <header className="fixed left-0 right-0 top-0 z-50 bg-light shadow-md dark:bg-dark">
       <section className="container flex h-[76px] items-center justify-between">
         <Link href="/">
           <Image
