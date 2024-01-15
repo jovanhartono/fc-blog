e78f2cad265b6a608da4e4352aef49c1be4ec8cf
jovan <jovanus@yahoo.co.id>
2024-01-15 20:21:57 +0700

    fix: mismatch theme between server and client

--- src/app/_components/theme-switcher.tsx
+++ src/app/_components/theme-switcher.tsx
@@ -12,11 +12,8 @@ export default function ThemeSwitcher() {
       className="text-black dark:text-white"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {theme === "dark" ? (
-        <MoonIcon className="h-4 w-4" />
-      ) : (
-        <SunIcon className="h-4 w-4" />
-      )}
+      {theme === "dark" ? <MoonIcon className="h-4 w-4" /> : null}
+      {theme === "light" ? <SunIcon className="h-4 w-4" /> : null}
     </button>
   );
 }
