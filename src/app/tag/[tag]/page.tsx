98dede9cceb6353bf138d2ce5db54a218ab1c97a
jovan <jovanus@yahoo.co.id>
2024-01-24 13:54:35 +0700

    feat(category page): add filtered by keywords and pagination

--- src/app/tag/[tag]/page.tsx
+++ src/app/tag/[tag]/page.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from "react";
 import { Metadata } from "next";
 import { allArticles } from "contentlayer/generated";
 import { slug } from "github-slugger";
 
 import ArticlePagination from "@/app/_components/article-pagination";
 import TagList from "@/app/_components/home/tag-list";
+import Search from "@/app/_components/search";
 
 export async function generateStaticParams() {
   return Array.from(
@@ -22,18 +24,32 @@ export const metadata: Metadata = {
   description: `Pelajari lebih lanjut tentang melalui artikel terbaik kami`,
 };
 
-function filteredArticles(tag: string) {
-  return tag === "all"
-    ? allArticles
-    : allArticles
-        .map((article) => ({
-          ...article,
-          tags: article.tags.map((tag) => slug(tag)),
-        }))
-        .filter((article) => article.tags.includes(tag));
+function filteredArticles(tag: string, search?: string) {
+  const articles = search
+    ? allArticles.filter((article) =>
+        article.title.toLowerCase().includes(search.toLowerCase()),
+      )
+    : allArticles;
+
+  if (tag === "all") {
+    return articles;
+  }
+
+  return articles
+    .map((article) => ({
+      ...article,
+      tags: article.tags.map((tag) => slug(tag)),
+    }))
+    .filter((article) => article.tags.includes(tag));
 }
 
-export default function TagPage({ params }: { params: { tag: string } }) {
+export default function TagPage({
+  params,
+  searchParams,
+}: {
+  params: { tag: string };
+  searchParams: { search?: string };
+}) {
   return (
     <main className="container gap-12">
       <div className="space-y-6">
@@ -49,10 +65,21 @@ export default function TagPage({ params }: { params: { tag: string } }) {
 
         <hr className="h-0.5 w-full bg-dark" />
 
-        <TagList />
+        <div className="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between lg:gap-12">
+          <TagList className="basis-1/2" />
+          <Suspense
+            fallback={
+              <div className="h-[42px] w-full animate-pulse rounded-lg bg-gray-100 lg:basis-1/2" />
+            }
+          >
+            <Search className="lg:basis-1/2" />
+          </Suspense>
+        </div>
       </div>
 
-      <ArticlePagination articles={filteredArticles(params.tag)} />
+      <ArticlePagination
+        articles={filteredArticles(params.tag, searchParams.search)}
+      />
     </main>
   );
 }
