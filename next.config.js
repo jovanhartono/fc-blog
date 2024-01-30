cf00063a3b92e52d6c0dd4a27c54af74e0b6e8ad
jovan <jovanus@yahoo.co.id>
2024-01-30 22:29:30 +0700

    chore(next-config): add remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fresclean.id",
+        port: "",
+        pathname: "/wp-content/uploads/**",
+      },
+    ],
+  },
+};
 
 module.exports = withContentlayer({ ...nextConfig });
