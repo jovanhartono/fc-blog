574f1e5b3578a4f4076058974527a3f432c99300
jovan <jovanus@yahoo.co.id>
2024-02-03 12:54:33 +0700

    fix(theme-switcher): cls before react hydration

--- src/app/_components/theme-switcher.tsx
+++ src/app/_components/theme-switcher.tsx
@@ -12,19 +12,21 @@ export default function ThemeSwitcher() {
     setMounted(true);
   }, []);
 
-  if (!mounted) return;
+  /* the server will pre-render a skeleton,
+     which will be displayed to the user as they await the resolution of React hydration.
+  */
+  if (!mounted)
+    return (
+      <div className="size-4 animate-pulse rounded-lg bg-gray-100 lg:basis-1/2" />
+    );
 
   return (
     <button
       aria-label="theme-switcher"
-      className="text-black dark:text-white"
+      className="size-4 text-black *:size-4 dark:text-white"
       onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
     >
-      {resolvedTheme === "dark" ? (
-        <MoonIcon className="h-4 w-4" />
-      ) : (
-        <SunIcon className="h-4 w-4" />
-      )}
+      {resolvedTheme === "dark" ? <MoonIcon /> : <SunIcon />}
     </button>
   );
 }
