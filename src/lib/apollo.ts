ac21a3a643012e94d1a19194a5494d223f428d5f
jovan <jovanus@yahoo.co.id>
2024-07-05 15:14:43 +0700

    upgrade apollo client

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,13 +1,13 @@
 import { HttpLink } from "@apollo/client";
-import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 import {
-  NextSSRApolloClient,
-  NextSSRInMemoryCache,
-} from "@apollo/experimental-nextjs-app-support/ssr";
+  ApolloClient,
+  InMemoryCache,
+  registerApolloClient,
+} from "@apollo/experimental-nextjs-app-support";
 
 export const { getClient } = registerApolloClient(() => {
-  return new NextSSRApolloClient({
-    cache: new NextSSRInMemoryCache(),
+  return new ApolloClient({
+    cache: new InMemoryCache(),
     link: new HttpLink({
       uri: process.env.NEXT_PUBLIC_WORDPRESS_API_URL,
     }),
